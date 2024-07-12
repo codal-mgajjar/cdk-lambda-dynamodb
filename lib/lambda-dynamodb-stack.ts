0fc79b7b16cc445a8a5823280e206b8059ad5e71
codal-mgajjar <mgajjar@codal.com>
2024-07-12 15:28:26 +0530

    cdk for lambda and dynamodb

--- lib/lambda-dynamodb-stack.ts
+++ lib/lambda-dynamodb-stack.ts
@@ -1,4 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
+import { ApiKey, ApiKeySourceType, Cors, LambdaIntegration, RestApi, UsagePlan } from 'aws-cdk-lib/aws-apigateway';
+import { PartitionKey } from 'aws-cdk-lib/aws-appsync';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +10,80 @@ export class LambdaDynamodbStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Dynamodb Table
+    const dbTable = new Table(this, 'DbTable', {
+      partitionKey: {name: 'pk', type: AttributeType.STRING},
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaDynamodbQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    //API Gateway
+    const api = new RestApi(this, 'RestAPI', {
+      restApiName: 'RestAPI',
+      defaultCorsPreflightOptions:{
+        allowOrigins: Cors.ALL_ORIGINS,
+        allowMethods: Cors.ALL_METHODS,
+      },
+      apiKeySourceType: ApiKeySourceType.HEADER
+    });
+
+    //Api Key
+    const apiKey = new ApiKey(this, 'ApiKey');
+
+    //UsagePlan 
+    const usagePlan = new UsagePlan(this, 'UsagePlan', {
+      name: 'Usage Plan',
+      apiStages: [
+        {
+          api,
+          stage:api.deploymentStage,
+        }
+      ]
+    });
+    usagePlan.addApiKey(apiKey);
+    
+    //lambda function 1
+    const postsLambda = new NodejsFunction(this, 'PostsLambda', {
+      entry: 'resources/endpoints/posts.ts',
+      handler: 'handler',
+      environment: {
+        TABLE_NAME: dbTable.tableName,
+      }
+    });
+
+    //Lambda function 2
+    const postLambda = new NodejsFunction(this, 'PostLambda', {
+      entry: 'resources/endpoints/post.ts',
+      handler: 'handler', 
+      environment:{
+        TABLE_NAME: dbTable.tableName,
+      }
+    });
+
+    dbTable.grantReadWriteData(postsLambda);
+    dbTable.grantReadWriteData(postLambda);
+
+    const posts = api.root.addResource('posts');
+    const post = posts.addResource('{id}');
+
+    const postsIntegration = new LambdaIntegration(postsLambda);
+    const postIntegration = new LambdaIntegration(postLambda);
+
+    posts.addMethod('GET', postsIntegration, {
+      apiKeyRequired: true,
+    });
+    posts.addMethod('POST', postsIntegration, {
+      apiKeyRequired: true,
+    });
+    post.addMethod('GET', postIntegration, {
+      apiKeyRequired: true,
+    });
+    post.addMethod('DELETE', postIntegration, {
+      apiKeyRequired: true,
+    });
+
+    new cdk.CfnOutput(this, 'API Key ID', {
+      value: apiKey.keyId,
+    });
   }
 }
